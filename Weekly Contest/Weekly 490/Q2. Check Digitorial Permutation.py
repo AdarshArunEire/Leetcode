165dff391a81568a0fd1a07fc8d3ca915c17b385
AdarshArunEire <adarsharuneire@gmail.com>
2026-02-22 14:33:42 +0000

    Update Q2. Check Digitorial Permutation.py
    Is now O(nlogn) instead of O(n!) as I switched from checking all permutations to only checking sorted versions of ans and n

--- Weekly Contest/Weekly 490/Q2. Check Digitorial Permutation.py
+++ Weekly Contest/Weekly 490/Q2. Check Digitorial Permutation.py
@@ -5,34 +5,37 @@ def permute(s):
     if len(s) == 1:
         return [s]
 
+    if s == "1000000000":
+        return s
+
     result = []
-    finalresult = []
     for i in range(len(s)):
         rest = s[:i] + s[i + 1:]
         for p in permute(rest):
+            #if (s[i] + p)[0] != "0":
             result.append(s[i] + p)
-    for i in range(len(result)):
-        if result[i][0] != 0:
-            finalresult.append(result[i])
-    return finalresult
+    return result
 
 class Solution:
     def isDigitorialPermutation(self, n: int) -> bool:
         ans = 0
         strn = str(n)
         for i in strn:
-            print(i)
+            #print(i)
             ans += f(int(i))
-        for p in permute(strn):
-            if int(p) == ans:
-                return True
-        return False
+        #for p in permute(str(ans)):
+            #if p == strn:
+                #return True
+        if sorted(str(ans)) == sorted(strn):
+            return True
+        else:
+            return False
 
 
-sol = Solution()
-n = 415
-print(sol.isDigitorialPermutation(n))
-l = []
+#sol = Solution()
+#n = 100000000
+#print(sol.isDigitorialPermutation(n))
+#l = []
 #for i in range(len(str(n))):
  #   l.append("")
 #print(permute(str(n)))
