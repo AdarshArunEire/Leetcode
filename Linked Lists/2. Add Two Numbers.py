5f4a7656003af9edf31448e27350e41176fbad76
AdarshArunEire <adarsharuneire@gmail.com>
2026-02-23 01:08:45 +0000

    Final Ver
    Will do more linked list questions to reinforce what I have learned

--- Linked Lists/2. Add Two Numbers.py
+++ Linked Lists/2. Add Two Numbers.py
@@ -1,113 +1,64 @@
 # Definition for singly-linked list.
-class Listnode:
+"""
+class ListNode:
      def __init__(self, val=0, next=None):
          self.val = val
          self.next = next
-
 
 def build_LL(arr):
     if not arr:
         return None
 
-    head = Listnode(arr[0])
+    head = ListNode(arr[0])
     current = head
 
     for val in arr[1:]:
-        current.next = Listnode(val)
+        current.next = ListNode(val)
         current = current.next
 
     return head
-
-
-def build_carry_list(l1, l2, carry_list, llen):
-    curr_l1 = l1
-    curr_l2 = l2
-    for _ in range(llen):
-        carry_list.insert(0, curr_l1.val + curr_l2.val)
-        curr_l1 = curr_l1.next
-        curr_l2 = curr_l2.next
-
-    curr_carried = 0
-    for i in range(len(carry_list)):
-        curr_carried = int(carry_list[i] + curr_carried > 10)
-        carry_list[i] = curr_carried
-
+"""
+def add(val1, val2, carry):
+    addsum = val1 + val2 + carry
+    if addsum >= 10:
+        carry = addsum // 10
+        addsum -= 10 * carry
+    else:
+        carry = 0
+    return addsum, carry
 
 class Solution:
-    def addTwoNumbers(self, l1: [Listnode], l2: [Listnode]):
-        ans = []
+    def addTwoNumbers(self, l1: [ListNode], l2: [ListNode]):
+        carry = 0
+        ans = ListNode(0)
+        curr_ans = ans
+        curr_l1 = l1
+        curr_l2 = l2
+        while curr_l1 or curr_l2 or carry:
+            if curr_l1 and curr_l2:
+                addsum, carry = add(curr_l1.val, curr_l2.val, carry)
+                curr_l1 = curr_l1.next
+                curr_l2 = curr_l2.next
+            elif curr_l1:
+                addsum, carry = add(curr_l1.val, 0, carry)
+                curr_l1 = curr_l1.next
+            elif curr_l2:
+                addsum, carry = add(0, curr_l2.val, carry)
+                curr_l2 = curr_l2.next
+            else:
+                addsum = carry
+                carry = 0
+            curr_ans.next = ListNode(addsum)
+            curr_ans = curr_ans.next
+        return ans.next
 
-        curr = l1
-        lenl1 = 0
-        l1last = None
-        while curr:
-            lenl1 += 1
-            l1last = curr
-            curr = curr.next
-
-        curr = l2
-        lenl2 = 0
-        l2last = None
-        while curr:
-            lenl2 += 1
-            l2last = curr
-            curr = curr.next
-
-        max_len = max(lenl1, lenl2)
-        for d in range(max_len - lenl1):
-            l1last.next = Listnode(0)
-            l1last = l1last.next
-        for d in range(max_len - lenl2):
-            l2last.next = Listnode(0)
-            l2last = l2last.next
-
-        carry = build_carry_list(l1, l2, [], max_len)
-
-'''
-def print_LL(LL):
-    curr = LL
-    while curr:
-        print(curr.val, end = ">")
-        curr = curr.next
-    print(" ")
-
-l1 = build_LL([2, 4])
+"""
+sol = Solution()
+l1 = build_LL([2, 4, 3])
 l2 = build_LL([5, 6, 4])
-curr = l1
-lenl1 = 0
-l1last = None
 
+curr = sol.addTwoNumbers(l1, l2)
 while curr:
-    lenl1 += 1
-    l1last = curr
+    print(curr.val, end=">")
     curr = curr.next
-
-curr = l2
-lenl2 = 0
-l2last = None
-while curr:
-    lenl2 += 1
-    l2last = curr
-    curr = curr.next
-
-max_len = max(lenl1, lenl2)
-for d in range(max_len - lenl1):
-    l1last.next = Listnode(0)
-    l1last = l1last.next
-for d in range(max_len - lenl2):
-    l2last.next = Listnode(0)
-    l2last = l2last.next
-
-#print_LL(l1)
-#print_LL(l2)
-
-print(build_carry_list(l1, l2, [], max_len))
-'''
-
-carry_list = [4, 10, 9]
-curr_carried = 0
-for i in range(len(carry_list)):
-    curr_carried = int(carry_list[i] + curr_carried >= 10)
-    carry_list[i] = curr_carried
-
-print(carry_list)
+"""
